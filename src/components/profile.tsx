a812d98c701cff47c54c10264ccaabb9efc95f4f
Mohammed Adil <47940247+ShadowTitan123@users.noreply.github.com>
2022-09-12 20:28:45 +0530

    feat: installed mui dependencies, theme setup , components setup , initiated homepage design (#1)
    Co-authored-by: Mohammed Adil <mohammed.adil@optym.com>

---
+++ src/components/profile.tsx
@@ -0,0 +1,12 @@
+import Paper from '@mui/material/Paper';
+
+const Profile = () => {
+  return (
+    <Paper elevation={3} sx={{ height: '200px', width: '800px' }}>
+      {' '}
+      Profile Content
+    </Paper>
+  );
+};
+
+export default Profile;
--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,9 @@
  * Application related global configurations.
  */
 
+import { lightTheme } from './core/theme/lightTheme';
+
 export default {
   appName: 'Optym5',
+  appTheme: lightTheme,
 };
